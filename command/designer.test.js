f02b1ef4ac56488175187ef43bf1b3ec23664aa6
NEUROSYN-DEV <lilsynn89@gmail.com>
2026-09-15 16:47:00 -0700

    test: cover designer asset tools

--- command/designer.test.js
+++ command/designer.test.js
@@ -22,12 +22,21 @@ assert.equal(sandbox.window.LSDesignerTestHooks.toolRegistryValid(), true);
 
 const plusSource = fs.readFileSync('command/designer-plus.js', 'utf8');
 vm.runInContext(plusSource, sandbox);
-assert.equal(sandbox.window.LSDesignerPlus.version, 1, 'advanced designer tools should expose a versioned API');
+assert.equal(sandbox.window.LSDesignerPlus.version, 1, 'advanced designer should expose a versioned API');
 assert.equal(typeof sandbox.window.LSDesignerPlus.addElement, 'function', 'advanced designer should support element insertion');
 assert.equal(typeof sandbox.window.LSDesignerPlus.move, 'function', 'advanced designer should support nudging');
 assert.equal(typeof sandbox.window.LSDesignerPlus.align, 'function', 'advanced designer should support alignment');
 
+const assetSource = fs.readFileSync('command/designer-assets.js', 'utf8');
+vm.runInContext(assetSource, sandbox);
+assert.equal(sandbox.window.LSDesignerAssets.version, 1, 'asset tools should expose a versioned API');
+assert.equal(typeof sandbox.window.LSDesignerAssets.insertAsset, 'function');
+assert.equal(typeof sandbox.window.LSDesignerAssets.duplicateDom, 'function');
+assert.equal(typeof sandbox.window.LSDesignerAssets.deleteDom, 'function');
+
 const html = fs.readFileSync('command/designer.html', 'utf8');
 assert.match(html, /designer-plus\.js/, 'designer page should load advanced tools');
+assert.match(html, /designer-assets\.js/, 'designer page should load asset tools');
+assert.match(html, /designer-assets\.css/, 'designer page should load asset styles');
 assert.match(html, /LIL SYNN \/ DESIGNER/, 'designer branding should remain present');
 console.log('designer contracts: PASS');
