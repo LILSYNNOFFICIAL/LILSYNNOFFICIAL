6e4cd53cf5e2692cdc88e93eb98f4bebbcd2de66
NEUROSYN-DEV <lilsynn89@gmail.com>
2026-09-15 16:45:07 -0700

    test: cover expanded visual designer tool contract

--- command/designer.test.js
+++ command/designer.test.js
@@ -19,4 +19,15 @@ assert.equal(sandbox.window.LSDesignerTestHooks.historyRoundTrip(), true);
 assert.equal(sandbox.window.LSDesignerTestHooks.patchRoundTrip(), true);
 assert.equal(sandbox.window.LSDesignerTestHooks.draftRoundTrip(), true);
 assert.equal(sandbox.window.LSDesignerTestHooks.toolRegistryValid(), true);
+
+const plusSource = fs.readFileSync('command/designer-plus.js', 'utf8');
+vm.runInContext(plusSource, sandbox);
+assert.equal(sandbox.window.LSDesignerPlus.version, 1, 'advanced designer tools should expose a versioned API');
+assert.equal(typeof sandbox.window.LSDesignerPlus.addElement, 'function', 'advanced designer should support element insertion');
+assert.equal(typeof sandbox.window.LSDesignerPlus.move, 'function', 'advanced designer should support nudging');
+assert.equal(typeof sandbox.window.LSDesignerPlus.align, 'function', 'advanced designer should support alignment');
+
+const html = fs.readFileSync('command/designer.html', 'utf8');
+assert.match(html, /designer-plus\.js/, 'designer page should load advanced tools');
+assert.match(html, /LIL SYNN \/ DESIGNER/, 'designer branding should remain present');
 console.log('designer contracts: PASS');
